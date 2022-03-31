8e2dc0884b79cb232f751505a66680a13278065c
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-31 21:39:39 +0200

    add route to redirect home when no path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CreateBlogComponent } from './pages/create-blog/create-blog.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'home'},
   {path:'home', component: HomeComponent},
   {path:'create', component: CreateBlogComponent}
 ];
