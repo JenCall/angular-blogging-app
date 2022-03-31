1db4dc8c813ca19c8b01312abd4317dc31e5f9fc
Jeniffer Callou <jenifferprc@gmail.com>
2022-03-31 21:37:51 +0200

    add route home + create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBlogComponent } from './pages/create-blog/create-blog.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'create', component: CreateBlogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
