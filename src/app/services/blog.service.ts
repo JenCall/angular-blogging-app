60d03e0ad13e5771fb5c7554f0732371baae971e
Jeniffer Callou <jenifferprc@gmail.com>
2022-04-03 19:25:33 +0200

    add http to create a new blog

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public postBlog(data:any){
+    return new Promise<any>((resolve, reject)=>{
+      this.http.post('http://localhost:3000/blogs', data).subscribe(
+        (res)=>{
+          resolve(res);
+        },
+        (err)=>{
+          reject(err);
+        }
+      );
+    })
+  }
 }
